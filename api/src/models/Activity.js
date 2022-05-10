4625bafd2533a913284889ba7f84fa6272286c5d
Alejandroni <betanmendoza@gmail.com>
2022-05-09 19:21:01 -0500

    melo

--- api/src/models/Activity.js
+++ api/src/models/Activity.js
@@ -1,45 +1,33 @@
 const { DataTypes } = require('sequelize');
-const { defaultValueSchemable } = require('sequelize/types/utils');
+//const { defaultValueSchemable } = require('sequelize/types/utils');
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
-  // defino el modelo
-  sequelize.define('activity', {
-/*
-ID
-Nombre
-Dificultad (Entre 1 y 5)
-Duración
-Temporada (Verano, Otoño, Invierno o Primavera)
-*/
-/*
-id:{
-  type: DataTypes.CHAR, 
+    // defino el modelo
+    sequelize.define('activity', {
+      
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        level: {
+            type: DataTypes.ENUM({
+                values: ['1', '2', '3', '4', '5']
+            }),
+            allowNull: false,
+        },
+        duration: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        season: {
+            type: DataTypes.ENUM({
+                valores: ["Verano", "Otoño", "Invierno", "Primavera"]
+            }),
+            allowNull: false,
 
-allowNull: false,
-primaryKey: true,
-},*/
-nombre: {
-  type: DataTypes.STRING,
-  allowNull: false,
-},
-dificultad: {
-  type: DataTypes.ENUM({
-      valores: ["1","2","3","4","5"]
-  }),
-  allowNull: false,
-},
-duracion: {
-  type: DataTypes.STRING,
-  allowNull: false,
-},
-temporada: {
-type: DataTypes.ENUM({
-    valores: ["Verano","Otoño","Invierno","Primavera"]
-})
-
-},
+        },
 
 
-  });
+    });
 };
